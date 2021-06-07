0ac187e4dcc6fa51ff461bb7410be91f65a3bb2a
Rafał Borcz <rafal.borcz@goodylabs.com>
2021-06-07 14:22:38 +0200

    Initial commit

---
+++ app/src/main/java/com/goodylabs/android/interview/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.goodylabs.android.interview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
