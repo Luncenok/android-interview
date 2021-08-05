a4de3ab573c1486e613a45947062e1156e331c4b
Luncenok <idziejczak3@gmail.com>
2021-08-05 15:12:35 +0200

    Add loading animations in CharacterListScreen

--- app/src/main/java/com/goodylabs/android/interview/ui/characterlist/CharacterListScreen.kt
+++ app/src/main/java/com/goodylabs/android/interview/ui/characterlist/CharacterListScreen.kt
@@ -1,28 +1,81 @@
 package com.goodylabs.android.interview.ui.characterlist
 
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberImagePainter
 import coil.transform.CircleCropTransformation
+import com.goodylabs.android.interview.data.models.Character
+
+data class CharacterListUiState(
+    val characterDetails: List<Character>? = null,
+    val isLoading: Boolean = false,
+    val throwError: Boolean = false
+)
 
 @Composable
 fun CharacterListBody(
     onCharacterClick: (Int) -> Unit,
     viewModel: CharacterListViewModel = viewModel()
 ) {
-    val listItems = viewModel.listCharacters.observeAsState().value ?: emptyList()
+
+    val listItems = viewModel.listCharacters.observeAsState()
+    val errorText = viewModel.errorText.observeAsState()
+
+    val uiState = when {
+        listItems.value.isNullOrEmpty() -> CharacterListUiState(isLoading = true)
+        !errorText.value.isNullOrBlank() -> CharacterListUiState(throwError = true)
+        else -> CharacterListUiState(characterDetails = listItems.value)
+    }
+
+    when {
+        uiState.characterDetails != null -> {
+            CharacterListContent(
+                onCharacterClick = onCharacterClick,
+                viewModel = viewModel,
+                listItems = uiState.characterDetails
+            )
+        }
+        uiState.isLoading -> {
+            LazyColumn {
+                items(20) {
+                    CharacterListLoadingItem()
+                }
+            }
+        }
+        else -> {
+            Text(
+                text = "An error occurred. Please restart app to try again",
+                modifier = Modifier.padding(30.dp),
+                style = MaterialTheme.typography.h3
+            )
+        }
+    }
+}
+
+@Composable
+fun CharacterListContent(
+    onCharacterClick: (Int) -> Unit,
+    viewModel: CharacterListViewModel,
+    listItems: List<Character>
+) {
     val scrollState = rememberLazyListState()
 
     LazyColumn(state = scrollState) {
@@ -53,10 +106,10 @@ fun CharacterListItem(
 ) {
     Row(
         modifier = modifier
+            .padding(10.dp)
             .fillMaxWidth()
             .height(75.dp)
-            .clickable(onClick = { onCharacterClick(characterId) })
-            .padding(10.dp),
+            .clickable(onClick = { onCharacterClick(characterId) }),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Image(
@@ -71,6 +124,44 @@ fun CharacterListItem(
             modifier = Modifier.padding(horizontal = 10.dp),
             style = MaterialTheme.typography.body1,
             text = characterName
+        )
+    }
+}
+
+@Composable
+fun CharacterListLoadingItem() {
+
+    val infiniteTransition = rememberInfiniteTransition()
+    val alpha by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(
+            animation = keyframes {
+                durationMillis = 1000
+                0.7f at 500
+            },
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+    Row(
+        modifier = Modifier
+            .padding(10.dp)
+            .fillMaxWidth()
+            .height(75.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(
+            modifier = Modifier
+                .size(75.dp)
+                .clip(CircleShape)
+                .background(Color.LightGray.copy(alpha = alpha))
+        )
+        Spacer(Modifier.width(10.dp))
+        Box(
+            modifier = Modifier
+                .background(Color.LightGray.copy(alpha = alpha))
+                .width(150.dp)
+                .height(16.dp)
         )
     }
 }
